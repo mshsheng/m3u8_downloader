a2ef6325c9943ba5f833edd518e7a26efb0a60eb
MaZesheng <mshsheng@126.com>
2018-11-07 16:00:37 +0800

    delete m3u8 cache path

--- download.py
+++ download.py
@@ -6,7 +6,6 @@ ssl._create_default_https_context = ssl._create_unverified_context
 
 def m3u8_download(m3u8_url,headers):
     req = urllib.request.Request(m3u8_url,headers=headers)
-    path = 'temp.m3u8'
     data = urllib.request.urlopen(req).read()
     data_utf8 = data.decode('utf8')
     m3u8_content = data_utf8.split('\n')
